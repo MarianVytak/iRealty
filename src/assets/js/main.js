78fdc18af61de263fd0fa5e29aac270c65cc035f
MarianVytak <marianvytak@gmail.com>
2020-08-28 16:07:58 +0300

    Added Home 2.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,2 +1,4 @@
+//= components/card.js
 //= components/customs.js
 //= ../../templates/intro/intro.js
+//= ../../templates/home-2/home-2.js
---
+++ src/templates/home-2/home-2.js
@@ -0,0 +1,10 @@
+$('.home-2__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: false,
+    fade: false,
+    arrows: true,
+    dots: false,
+    prevArrow: $('.home-2__arrow_prev'),
+    nextArrow: $('.home-2__arrow_next')
+});
