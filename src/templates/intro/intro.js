7cdce1bfb657e0352767aad8fc12ab09e7b80d2b
MarianVytak <marianvytak@gmail.com>
2020-09-09 00:08:39 +0300

    Added Intro Consist.

--- src/templates/intro/intro.js
+++ src/templates/intro/intro.js
@@ -11,6 +11,7 @@ $('.intro__slider').slick({
     dots: true
 });
 
+
 // Slide 2 Slider
 $('.slide-2__slider').slick({
     slidesToShow: 1,
@@ -24,14 +25,32 @@ $('.slide-2__slider').slick({
     dots: true
 });
 
+
 // Intro Form Dropdown Text
-$('.intro__form .dropdown-item').on('click', function (e){
+$('.intro__form .dropdown-item').on('click', function (e) {
     e.preventDefault();
     let thisValue = $(this).attr('href');
     $(thisValue).html($(this).html());
 });
 
-$('.intro__form_price_range input').on('change', function (){
+$('.intro__form_price_range input').on('change', function () {
     let thisInput = $(this).attr('data-target');
     $(thisInput).text($(this).val());
+});
+
+
+// Change Intro Consist
+$('.increment').click(function () {
+    let valueElement = $('#' + $(this).siblings('input').attr('id'));
+    if ($(this).hasClass('plus')) {
+        valueElement.val(Math.max(parseInt(valueElement.val()) + 1));
+    } else if (valueElement.val() > 0) // Stops the value going into negatives
+    {
+        valueElement.val(Math.max(parseInt(valueElement.val()) - 1));
+    }
+    return false;
+});
+$('.intro__consist_btn').on('click', function (){
+    $('#introConsistParent').text($('#consistParent').val());
+    $('#introConsistChildren').text($('#consistChildren').val());
 });
