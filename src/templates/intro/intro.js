1d44c78f10fb9166ba3b7ce21fef016486479b10
MarianVytak <marianvytak@gmail.com>
2020-09-08 14:36:32 +0300

    Intro Form: added price range.

--- src/templates/intro/intro.js
+++ src/templates/intro/intro.js
@@ -23,3 +23,15 @@ $('.slide-2__slider').slick({
     arrows: true,
     dots: true
 });
+
+// Intro Form Dropdown Text
+$('.intro__form .dropdown-item').on('click', function (e){
+    e.preventDefault();
+    let thisValue = $(this).attr('href');
+    $(thisValue).html($(this).html());
+});
+
+$('.intro__form_price_range input').on('change', function (){
+    let thisInput = $(this).attr('data-target');
+    $(thisInput).text($(this).val());
+});
